59c25d01d0c8a4fdb051dfccd5e786b69c7d9885
Alex <ildarkin.alexey@gmail.com>
2016-04-07 12:57:05 +0300

    Немного подрефакторил код

--- Lab3/Calculator/Calculator/Additions.h
+++ Lab3/Calculator/Calculator/Additions.h
@@ -11,10 +11,13 @@ struct FunctionParamethers
 	std::string value2 = "";
 };
 
-struct StrLess : public std::binary_function<std::string, std::string, bool>
+struct StrComparison : public std::binary_function<std::string, std::string, bool>
 {
-	bool const operator () (const std::string &str1, const std::string &str2) { return _stricoll(str1.c_str(), str2.c_str()) < 0; }
+	bool const operator () (const std::string &str1, const std::string &str2) 
+	{ 
+		return _stricoll(str1.c_str(), str2.c_str()) < 0;
+	}
 };
 
-typedef std::map<std::string, double, StrLess> VarsArray;
-typedef std::map<std::string, FunctionParamethers, StrLess> FunctionsArray;
+typedef std::map<std::string, double, StrComparison> VarsArray;
+typedef std::map<std::string, FunctionParamethers, StrComparison> FunctionsArray;
